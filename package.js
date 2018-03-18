574a23d3a4b5a5dc35c3ae569c236894608a625c
stefanocudini <stefano.cudini@gmail.com>
2018-03-18 16:34:43 +0100

    update ver

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name: 'keplerjs:wunderground',
   summary: 'keplerjs plugin for wunderground API',
-  version: "1.2.5",
+  version: "1.2.6",
   git: "https://github.com/Keplerjs/Kepler.git"
 });
 
@@ -11,16 +11,16 @@ Package.onUse(function(api) {
   api.versionsFrom("1.5.1");
 
   api.use([
-    'keplerjs:core@1.2.3',
+    'keplerjs:core@1.2.4',
   ]);
   
   api.addFiles([
     'plugin.js',
     'i18n/it.js',
     'i18n/en.js',
-	'i18n/de.js',
-	'i18n/es.js',
-	'i18n/fr.js',	
+    'i18n/de.js',
+    'i18n/es.js',
+    'i18n/fr.js',	
   ]);
 
   api.addFiles([
